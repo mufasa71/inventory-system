ff93be1a048e83fc4754eb76e9a61b491281b632
Shukhrat Mukimov <mukimov@gmail.com>
2012-07-14 14:15:01 +0800

    Change in stock

--- src/Stock.hs
+++ src/Stock.hs
@@ -1,14 +1,34 @@
 -- Stock representation
-module Stock
+module Stock where
+import System.IO
+import Control.Monad
 
--- Synonims
+-- Synonyms
 type StockCode = Int
 type UnitPrice = Double
 type DiscountPrice = Double
 type Description = String
 
+stock_db = "db/stock_db"
+
 data Stock = Stock { stockCode :: StockCode,
                    unitPrice :: UnitPrice,
                    discountPrice :: DiscountPrice,
-                   description :: Description 
-                 } deriving (Show, Read)
+                   description :: Description } deriving (Show, Read)
+
+loadStock :: FilePath -> IO ([Stock])
+loadStock path = do
+  file <- readFile path
+  let list' = [a | a <- map read (lines file) :: [Stock]]
+  return (list')
+
+printStock :: IO ([Stock]) -> IO [()]
+printStock input = do
+  stockList <- input  
+  putStrLn "Stock Code  |Price\t|Discount Price\t|Description"
+  putStrLn "_____________________________________________________"
+  prettyPrintedStock <- forM stockList (\item -> do
+      return $ show (stockCode item) ++ "\t    |$ " ++ show (unitPrice item) ++ "\t|$ " ++ show (discountPrice item) ++ "\t\t|" ++ show (description item))
+  mapM putStrLn prettyPrintedStock
+
+test = printStock $ loadStock stock_db

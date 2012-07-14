b416f2f351d8671fb1b5669eda4a06528e757c10
Shukhrat Mukimov <mukimov@gmail.com>
2012-07-14 17:10:24 +0800

    Added create and remove units

--- src/Stock.hs
+++ src/Stock.hs
@@ -1,7 +1,10 @@
 -- Stock representation
 module Stock where
 import System.IO
+import System.Directory
+import Data.List
 import Control.Monad
+import Control.Exception
 
 -- Synonyms
 type StockCode = Int
@@ -22,13 +25,60 @@ loadStock path = do
   let list' = [a | a <- map read (lines file) :: [Stock]]
   return (list')
 
-printStock :: IO ([Stock]) -> IO [()]
+printStock :: IO ([Stock]) -> IO ()
 printStock input = do
   stockList <- input  
   putStrLn "Stock Code  |Price\t|Discount Price\t|Description"
   putStrLn "_____________________________________________________"
-  prettyPrintedStock <- forM stockList (\item -> do
-      return $ show (stockCode item) ++ "\t    |$ " ++ show (unitPrice item) ++ "\t|$ " ++ show (discountPrice item) ++ "\t\t|" ++ show (description item))
-  mapM putStrLn prettyPrintedStock
+  prettyPrintedStock <- forM stockList (\unit -> do
+      return $ show (stockCode unit) ++ "\t    |$ " ++ show (unitPrice unit) ++ "\t|$ " ++ show (discountPrice unit) ++ "\t|" ++ show (description unit))
+  mapM_ putStrLn prettyPrintedStock
 
+addUnit :: IO (Stock)
+addUnit = do
+  putStrLn "Enter description: "
+  description <- getLine
+  putStrLn "Enter price: "
+  price <- getLine
+  putStrLn "Enter discount price: "
+  disPrice <- getLine
+  stock <- loadStock stock_db
+  let code = (+1) $ stockCode $ last stock
+  return $ Stock{stockCode=code, unitPrice=read price::Double, discountPrice=read disPrice::Double, description=description}
+
+saveStock :: Stock -> IO ()
+saveStock stock = do
+  contents <- readFile stock_db
+  let oldStockList = lines contents
+      newStockList = unlines $ oldStockList ++ [(show stock)]
+  (tempName, tempHandle) <- openTempFile "db" "temp"
+  hPutStr tempHandle newStockList
+  hClose tempHandle
+  removeFile "db/stock_db"
+  renameFile tempName "db/stock_db"
+
+removeUnit = do
+  contents <- readFile stock_db
+  let oldStockList = lines contents
+      numberedStock = zipWith (\n line -> show n ++ " - " ++ line) [0..] oldStockList
+  putStrLn "These are stock units: "
+  mapM_ putStrLn numberedStock
+  putStrLn "Which one you want to delete?"
+  numberString <- getLine
+  let number = read numberString
+      newStockList = unlines $ delete (oldStockList !! number) oldStockList
+  bracketOnError (openTempFile "db" "temp")
+    (\(tempName, tempHandle) -> do
+      hClose tempHandle
+      removeFile tempName)
+    (\(tempName, tempHandle) -> do
+      hPutStr tempHandle newStockList
+      hClose tempHandle
+      removeFile "db/stock_db"
+      renameFile tempName "db/stock_db")
+        
 test = printStock $ loadStock stock_db
+test2 = do
+  stock <- addUnit
+  _ <- saveStock stock
+  putStrLn $ show stock
